b8b2ec049048ae091ef295c076fc6e250395c30e
Dmitry <proctvvv@gmail.com>
2024-08-15 16:43:30 +0300

    feat: UI imitate on render texture and plane DEMO

---
+++ Flytonica/Assets/Code/Internal/PlaneUIRaycaster.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+namespace Code.Internal
+{
+    public class PlaneUIRaycaster : MonoBehaviour
+    {
+        public Camera mainCamera;
+        public RectTransform ui;
+
+        void Update()
+        {
+            if (UnityEngine.Input.GetMouseButtonDown(0))
+                Raycast();
+        }
+
+        private void Raycast()
+        {
+            var ray = mainCamera.ScreenPointToRay(UnityEngine.Input.mousePosition);
+            
+            if (Physics.Raycast(ray, out var hit))
+            {
+                var textureCoordinates = hit.textureCoord;
+
+                var screenPosition = new Vector2(
+                    textureCoordinates.x * ui.rect.width,
+                    textureCoordinates.y * ui.rect.height
+                );
+
+                var pointerData = new PointerEventData(EventSystem.current)
+                {
+                    position = screenPosition
+                };
+
+                var results = new List<RaycastResult>();
+                var uiRaycaster = ui.GetComponent<GraphicRaycaster>();
+
+                if (uiRaycaster != null)
+                {
+                    uiRaycaster.Raycast(pointerData, results);
+
+                    if (results.Count > 0)
+                    {
+                        foreach (var result in results)
+                        {
+                            var button = result.gameObject.GetComponent<Button>();
+                            if (button == null) continue;
+                            EmulateButtonClick(button, pointerData);
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    Debug.LogError("UI камера не имеет компонента GraphicRaycaster.");
+                }
+            }
+        }
+
+        private void EmulateButtonClick(Component button, BaseEventData pointerData)
+        {
+            ExecuteEvents.Execute(button.gameObject, pointerData, ExecuteEvents.pointerClickHandler);
+        }
+    }
+}
