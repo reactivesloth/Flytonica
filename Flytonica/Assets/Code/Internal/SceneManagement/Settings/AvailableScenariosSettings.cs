5f68e89e21c851c1d6dcb5f25b5d73feda175401
Nikita Kolpakov <n.kolpakov@reactivesloth.com>
2024-08-19 14:47:17 +0300

    feat: scenario logic update

--- Flytonica/Assets/Code/Internal/SceneManagement/Settings/AvailableScenariosSettings.cs
+++ Flytonica/Assets/Code/Internal/SceneManagement/Settings/AvailableScenariosSettings.cs
@@ -5,6 +5,6 @@ namespace Code.Internal.SceneManagement
     [CreateAssetMenu(fileName = "Scenario List", menuName = "Flytoncia/Scenes/Available Scenario", order = 1)]
     public class AvailableScenariosSettings : ScriptableObject
     {
-        public ScenarioSettings[] maps;
+        public ScenarioSettings[] scenario;
     }
 }
--- Flytonica/Assets/Code/Internal/SceneManagement/Settings/ScenarioSettings.cs
+++ Flytonica/Assets/Code/Internal/SceneManagement/Settings/ScenarioSettings.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using JetBrains.Annotations;
+using UnityEngine;
 
 namespace Code.Internal.SceneManagement
 {
@@ -6,5 +7,34 @@ namespace Code.Internal.SceneManagement
     public class ScenarioSettings : ScriptableObject
     {
         public new string name;
+        public ScenarioType scenarioType = ScenarioType.FreeFlight;
+        public MapSettings[] availableMaps;
+
+        [CanBeNull] public ScenarioSettings[] nestedScenarios;
+        [CanBeNull] public ScenarioSettings nextScenario;
+        
+        public string GetScenarioTypeName(ScenarioType type)
+        {
+            return type switch
+            {
+                ScenarioType.FreeFlight => $"Свободный полет",
+                ScenarioType.Tutorial => $"Обучение",
+                ScenarioType.Race => $"Гонка",
+                ScenarioType.Transport => $"Перевозка грузов",
+                ScenarioType.Searching => $"Поиск с помощью камеры",
+                ScenarioType.SearchingWithIR => $"Поиск с тепловизором",
+                _ => $"Пользовательский сценарий"
+            };
+        }
+    }
+
+    public enum ScenarioType
+    {
+        FreeFlight,
+        Tutorial,
+        Race,
+        Transport,
+        Searching,
+        SearchingWithIR
     }
 }
