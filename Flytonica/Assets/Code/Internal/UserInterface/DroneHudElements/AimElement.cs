e06d258cb17db68dde1f2fb0a930177b51c1d399
Dmitry <proctvvv@gmail.com>
2024-08-24 20:33:14 +0300

    feat: DroneHUD view and interface

---
+++ Flytonica/Assets/Code/Internal/UserInterface/DroneHudElements/AimElement.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.Internal.UserInterface.DroneHudElements
+{
+    public class AimElement: MonoBehaviour
+    {
+        [SerializeField] private Image progressImage;
+        [SerializeField] private Color targetColor = Color.green;
+        [SerializeField] private GameObject progress;
+        [SerializeField] private Image aimIcon, cameraIcon;
+
+        private Color _startColor;
+
+        private Coroutine _currentAnim;
+
+        private void Awake()
+        {
+            _startColor = progressImage.color;
+        }
+
+        public void SetProgressValue(float value)
+        {
+            value = Mathf.Clamp(value, 0f, 1f);
+
+            var isValue = value > 0;
+            progress.SetActive(isValue);
+            cameraIcon.gameObject.SetActive(isValue);
+            aimIcon.gameObject.SetActive(!isValue);
+            
+            progressImage.fillAmount = value;
+            progressImage.color = Color.Lerp(_startColor, targetColor, value);
+        }
+
+        public void Flash(Color color, float animTime, Action callback = null)
+        {
+            if(_currentAnim != null)
+                return;
+
+            _currentAnim = StartCoroutine(AnimFlash(color, animTime, callback));
+        }
+
+        private IEnumerator AnimFlash(Color color, float animTime, Action callback)
+        {
+            var sectionTime = animTime / 3;
+            var startColor = cameraIcon.color;
+            
+            var currentTime = 0f;
+            while (currentTime < sectionTime)
+            {
+                cameraIcon.color = Color.Lerp(_startColor, color, currentTime / sectionTime);
+                yield return null;
+                currentTime += Time.deltaTime;
+            }
+            
+            currentTime = 0f;
+            var currentColor = cameraIcon.color;
+            while (currentTime < sectionTime)
+            {
+                cameraIcon.color = Color.Lerp(currentColor, Color.clear, currentTime / sectionTime);
+                yield return null;
+                currentTime += Time.deltaTime;
+            }
+            
+            currentTime = 0f;
+            currentColor = cameraIcon.color;
+            while (currentTime < sectionTime)
+            {
+                cameraIcon.color = Color.Lerp(currentColor, startColor, currentTime / sectionTime);
+                yield return null;
+                currentTime += Time.deltaTime;
+            }
+            
+            _currentAnim = null;
+            cameraIcon.color = startColor;
+            callback?.Invoke();
+        }
+    }
+}
---
+++ Flytonica/Assets/Code/Internal/UserInterface/DroneHudElements/ValueElement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.Internal.UserInterface.DroneHudElements
+{
+    public class ValueElement : MonoBehaviour
+    {
+        [Header("Values: ")] [SerializeField] private int maxValue = 100;
+        [SerializeField] private Color normalColor, warningColor, extremeColor;
+        [Range(0, 1)] [SerializeField] private float warningPercent, extremePersent;
+
+        [Header("Elements: ")] [SerializeField]
+        private TMP_Text valueText;
+
+        [SerializeField] private Slider slider;
+
+        private Graphic _sliderFill;
+
+        public int MaxValue
+        {
+            get => maxValue;
+            set => maxValue = value;
+        }
+
+        private void Awake()
+        {
+            _sliderFill = slider.fillRect.GetComponent<Graphic>();
+        }
+
+        public void Set(float value)
+        {
+            var percent = value / maxValue;
+            SetColor(percent);
+            slider.value = percent;
+            valueText.text = value.ToString();
+        }
+
+        private void SetColor(float percent)
+        {
+            if (percent >= extremePersent)
+                valueText.color = _sliderFill.color = extremeColor;
+            else if (percent >= warningPercent)
+                valueText.color = _sliderFill.color = warningColor;
+            else
+                valueText.color = _sliderFill.color = normalColor;
+        }
+    }
+}
