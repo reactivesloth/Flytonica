ccf1b4d5b47693904924ec646bc837c4a605868d
Dmitry <proctvvv@gmail.com>
2024-09-09 21:53:38 +0300

    Feat: calibration page and process pc UI

---
+++ Flytonica/Assets/Code/Internal/UserInterface/Pages/CalibrationPage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.Internal.UserInterface.Pages
+{
+    public class CalibrationPage: Page
+    {
+        [SerializeField] private Button nextButton, recolibrateButton, saveButton;
+        [SerializeField] private GameObject calibrating, endCalibration;
+        [SerializeField] private List<Step> steps;
+        [SerializeField] private Slider gasSlider, rotateSlider, pitchSlider, rollSlider;
+
+        private int _currentStep = 0;
+        
+        protected override void OnBackClick()
+        {
+            base.OnBackClick();
+        }
+
+        protected override void OnOpen()
+        {
+            base.OnOpen();
+            
+        }
+
+        protected override void OnClose()
+        {
+            base.OnClose();
+            
+        }
+
+        private void NextStep()
+        {
+            _currentStep++;
+            
+        }
+    }
+
+    [Serializable]
+    public struct Step
+    {
+        public GameObject step, help;
+        public bool last;
+    }
+}
---
+++ Flytonica/Assets/Code/Internal/UserInterface/Pages/ScenariosPage.cs
@@ -0,0 +1,22 @@
+﻿using Code.Internal.UserInterface.Elements;
+using UnityEngine;
+
+namespace Code.Internal.UserInterface.Pages
+{
+    public class ScenariosPage: Page
+    {
+        [SerializeField] private Table scenariosTable;
+
+        protected override void OnOpen()
+        {
+            base.OnOpen();
+            
+            //TODO: Запрос и вызов GenerateTable
+        }
+
+        private void GenerateTable()
+        {
+            //TODO: Генерация таблицы 
+        }
+    }
+}
