96a6bb1ba26967f99e3167d23f72c561e8db570d
Nikita Kolpakov <n.kolpakov@reactivesloth.com>
2024-09-18 04:17:08 +0300

    feat: camera mode WIP (mouse scroll)

--- Flytonica/Assets/Code/Internal/Drone/DroneCamera.cs
+++ Flytonica/Assets/Code/Internal/Drone/DroneCamera.cs
@@ -7,6 +7,10 @@ namespace Code.Internal.Drone
     {
         [SerializeField] private GameObject cameraObject;
         private DroneInput _droneInput;
+        
+        [SerializeField] [Range(-45,90)] private float currentAngle;
+        [SerializeField] [Range(-45, 90)] private float minAngle;
+        [SerializeField] [Range(0,90)] private float maxAngle = 90;
 
         private void Awake()
         {
@@ -21,6 +25,14 @@ namespace Code.Internal.Drone
             
             if (cameraObject.activeSelf != _droneInput.DroneCam)
                 cameraObject.SetActive(_droneInput.DroneCam);
+
+            currentAngle = Mathf.Clamp(currentAngle, minAngle, maxAngle);
+            
+            currentAngle += Time.deltaTime * UnityEngine.Input.GetAxis("Mouse ScrollWheel") * 100;
+            
+            var rot = cameraObject.transform.localRotation;
+            rot = Quaternion.Euler(currentAngle, rot.y, rot.z);
+            cameraObject.transform.localRotation = rot;
         }
     }
 }
