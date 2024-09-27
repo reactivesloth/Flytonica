71ae8557cd259531c768bea5aae1065d101d180a
Nikita Kolpakov <n.kolpakov@reactivesloth.com>
2024-09-28 00:09:31 +0300

    feat: drone camera sensitivity

--- Flytonica/Assets/Code/Internal/Drone/DroneCamera.cs
+++ Flytonica/Assets/Code/Internal/Drone/DroneCamera.cs
@@ -35,7 +35,7 @@ namespace Code.Internal.Drone
 
             currentAngle = Mathf.Clamp(currentAngle, minAngle, maxAngle);
             
-            currentAngle += Time.deltaTime * UnityEngine.Input.GetAxis("Mouse ScrollWheel") * 100;
+            currentAngle += Time.deltaTime * UnityEngine.Input.GetAxis("Mouse ScrollWheel") * 1000;
             
             var rot = cameraObject.transform.localRotation;
             rot = Quaternion.Euler(currentAngle, rot.y, rot.z);
