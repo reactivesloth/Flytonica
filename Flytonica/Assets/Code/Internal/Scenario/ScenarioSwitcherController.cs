562044e578583306d8bf64d47526e0fef7ffdf35
Nikita Kolpakov <n.kolpakov@reactivesloth.com>
2024-10-17 13:27:17 +0300

    fix: pico hands disappear when back to menu

--- Flytonica/Assets/Code/Internal/Scenario/ScenarioSwitcherController.cs
+++ Flytonica/Assets/Code/Internal/Scenario/ScenarioSwitcherController.cs
@@ -32,9 +32,7 @@ namespace Code.Internal.Scenario
         }
         
         public void NextOrEnd(bool isFailed = false)
-        {
-            UIController.Instance.Unpause();
-            
+        {            
             if(_currentStatus != 2)
                 _currentStatus = isFailed ? 2 : 1;
             
