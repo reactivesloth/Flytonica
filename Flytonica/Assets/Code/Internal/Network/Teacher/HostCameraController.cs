005f279ab034f36268e3dcc9b743092b67ec3d3c
Dmitry <Dimidril@users.noreply.github.com>
2024-10-26 22:27:56 +0300

    feat: DroneHUD in net game in teacher view

--- Flytonica/Assets/Code/Internal/Network/Teacher/HostCameraController.cs
+++ Flytonica/Assets/Code/Internal/Network/Teacher/HostCameraController.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Code.Internal.Drone;
+using Code.Internal.UserInterface;
 using Code.Internal.XR;
 using FishNet.Object;
 using UnityEngine;
@@ -21,24 +23,35 @@ namespace Code.Internal.Network.Teacher
             playerFpvCameraControllers.ForEach(p => p.IsViewed = true);
         }
 
+        private void Update()
+        {
+            if(targetDrone)
+                targetDrone?.GetComponent<DroneSensors>()?.UpdateHud();
+        }
+
         public void SetTeacherView()
         {
             playerFpvCameraControllers.ForEach(p => p.IsViewed = true);
-            if(!targetDrone) return;
+            
+            if(!targetDrone) 
+                return;
             var fpvCamController = targetDrone.GetComponent<XRDisableHeadTrackingInFPV>();
-            if(!fpvCamController) return;
+            if(!fpvCamController) 
+                return;
+            targetDrone = null;
             fpvCamController.IsViewed = false;
+            DroneHUD.Instance.ShowHUD(false);
         }
         
         public void SetTargetDrone(NetworkObject drone)
         {
-            var fpvCamController = drone.GetComponent<XRDisableHeadTrackingInFPV>();
+            var fpvCamController = drone?.GetComponent<XRDisableHeadTrackingInFPV>();
             if(!fpvCamController)
                 return;
-
             targetDrone = drone;
             fpvCamController.IsViewed = true;
             playerFpvCameraControllers.ForEach(p => p.IsViewed = false);
+            DroneHUD.Instance.ShowHUD(true);
         }
 
         private void OnDisable()
