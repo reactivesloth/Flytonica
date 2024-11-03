f4ddfe5d108892e68006b14a0acf320540b8079b
Nikita Kolpakov <n.kolpakov@reactivesloth.com>
2024-11-03 08:40:54 +0300

    feat: scenarioinitializer added tutorial management

---
+++ Flytonica/Assets/Code/Internal/Scenario/Tutorial/ScenarioTutorial.cs
@@ -0,0 +1,58 @@
+using Code.Internal.SceneManagement;
+using Code.Internal.UserInterface;
+using UnityEngine;
+
+namespace Code.Internal.Scenario.Tutorial
+{
+    public class ScenarioTutorial : ScenarioBase
+    {
+        private float _timeTakeoff, _timeRacing;
+        
+        private void Start()
+        {
+            DroneHUD.Instance?.SetTask("Выполняйте задания обучения");
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+#if UNITY_EDITOR
+            if (UnityEngine.Input.GetKeyDown(KeyCode.Tab))
+            {
+                FinishRace();
+            }
+#endif
+        }
+
+        public override void Initialize(ScenarioSettings scenario)
+        {
+            base.Initialize(scenario);
+            
+            if (ScenarioCondition == ScenarioCondition.Waiting)
+            {
+                StartRace();
+            }
+        }
+
+        protected override void StartRace()
+        {
+            base.StartRace();
+            _timeTakeoff = 0;
+            _timeRacing = 0;
+        }
+
+        protected override void FinishRace(bool success = true)
+        {
+            base.FinishRace(success);
+        }
+
+        protected override void AddStatistic()
+        {
+            base.AddStatistic();
+
+            var resultBuilder = ReportBuilder.Instance;
+            resultBuilder.AddParameter($"Время на взлёт", GetTime(_timeTakeoff));
+            resultBuilder.AddParameter($"Время прохождения задания", GetTime(_timeRacing));
+        }
+    }
+}
