46a27bfc7e3467771b2f94fcd082f187830ee4bf
Nikita Kolpakov <n.kolpakov@reactivesloth.com>
2024-11-03 07:49:46 +0300

    feat: tablet disabled in menu

---
+++ Flytonica/Assets/Code/Internal/XR/XRPlayerHandItems.cs
@@ -0,0 +1,35 @@
+using Code.Internal.API;
+using Code.Internal.API.Wrappers;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class XRPlayerHandItems : MonoBehaviour
+{
+    [SerializeField] private GameObject tablet;
+    [SerializeField] private GameObject gamepad;
+
+    private void Awake()
+    {
+        SceneManager.activeSceneChanged += SceneChanged;
+    }
+
+    private void SceneChanged(Scene arg0, Scene arg1)
+    {
+        tablet.SetActive(false);
+        gamepad.SetActive(false);
+
+        if (!SceneManager.GetActiveScene().name.Equals("Main") && !SceneManager.GetActiveScene().name.Equals("UI"))
+        {
+            if (HttpClient.UserData.type == UserType.Teacher)
+            {
+                gamepad.SetActive(false);
+                tablet.SetActive(true);
+            }
+            else
+            {
+                tablet.SetActive(true);
+                gamepad.SetActive(true);
+            }
+        }
+    }
+}
