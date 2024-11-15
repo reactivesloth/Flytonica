d45e720a116825acd7c03ea9ad323a136532d8ab
Nikita Kolpakov <n.kolpakov@reactivesloth.com>
2024-11-15 09:18:57 +0300

    feat: drone controller add heightcheck

--- Flytonica/Assets/Code/Internal/Drone/DroneController.cs
+++ Flytonica/Assets/Code/Internal/Drone/DroneController.cs
@@ -323,7 +323,7 @@ namespace Code.Internal.Drone
             engineRR.UpdateEngine(_rigidBody, currentVoltage, acceleration, controlRl);
             engineRL.UpdateEngine(_rigidBody, currentVoltage, acceleration, controlRr);
             
-            if (_rigidBody.linearVelocity.magnitude < 1f && GetRPM() <= 0 && _throttle < 0.1f)
+            if (_rigidBody.linearVelocity.magnitude < 1f && GetRPM() <= 0 && _throttle < 0.1f && GetHeightFromFloor() < 1)
                 ResetEngines();
         }
 
@@ -453,6 +453,18 @@ namespace Code.Internal.Drone
             int pointCount = lineRenderer.positionCount;
             lineRenderer.positionCount = pointCount + 1;
             lineRenderer.SetPosition(pointCount, currentPosition);
+        }
+
+        public float GetHeightFromFloor()
+        {
+            if (Physics.Raycast(_transform.position, Vector3.down, out RaycastHit hit, Mathf.Infinity))
+            {
+                return hit.distance;
+            }
+            else
+            {
+                return Mathf.Infinity;
+            }
         }
     }
 }
