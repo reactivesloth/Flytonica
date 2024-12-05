50f3c28a4841cc6aeb9aa28d5ff5562b65717930
Nikita Kolpakov <n.kolpakov@reactivesloth.com>
2024-12-06 00:23:41 +0300

    fix: althold ceiling check ignore triggers

--- Flytonica/Assets/Code/Internal/Drone/DroneSensors.cs
+++ Flytonica/Assets/Code/Internal/Drone/DroneSensors.cs
@@ -176,7 +176,7 @@ namespace Code.Internal.Drone
 
         public float GetDistanceFromCeiling()
         {
-            if (Physics.Raycast(_droneController.transform.position + Vector3.up * 0.1f, Vector3.up, out RaycastHit hit, Mathf.Infinity))
+            if (Physics.Raycast(_droneController.transform.position + Vector3.up * 0.1f, Vector3.up, out RaycastHit hit, Mathf.Infinity, ~0, QueryTriggerInteraction.Ignore))
             {
                 return hit.distance;
             }
